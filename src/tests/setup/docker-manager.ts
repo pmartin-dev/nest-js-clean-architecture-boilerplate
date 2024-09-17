a2fa6f68165f9309bf092de00527c4f97c42cb74
PierreMartinFaberNovel <pierre.martin@fabernovel.com>
2024-09-17 17:30:23 +0200

    test: test container setup for integration tests

---
+++ src/tests/setup/docker-manager.ts
@@ -0,0 +1,38 @@
+import * as path from 'path';
+import {
+  DockerComposeEnvironment,
+  StartedDockerComposeEnvironment,
+} from 'testcontainers';
+
+let instance: StartedDockerComposeEnvironment | null = null;
+
+export const startDocker = async () => {
+  const composeFilePath = path.resolve(__dirname);
+  const compileFile = 'docker-compose.yml';
+
+  instance = await new DockerComposeEnvironment(
+    composeFilePath,
+    compileFile,
+  ).up();
+};
+
+export const stopDocker = async () => {
+  if (!instance) {
+    return;
+  }
+
+  try {
+    await instance.down();
+    instance = null;
+  } catch (e) {
+    console.error('Failed to stop docker : ', e);
+  }
+};
+
+export const getDockerEnvironment = (): StartedDockerComposeEnvironment => {
+  if (!instance) {
+    throw new Error('Instance is not available');
+  }
+
+  return instance;
+};
