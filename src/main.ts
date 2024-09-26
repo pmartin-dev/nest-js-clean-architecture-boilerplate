506bc941db061c38f65f2d9baf2869895a4f6a7c
MrPierreMartin <pierre.martin@fabernovel.com>
2024-09-26 10:08:36 +0200

    chore: docker mongo for start

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log(`🚀 Started on port 3000`);
 }
 bootstrap();
